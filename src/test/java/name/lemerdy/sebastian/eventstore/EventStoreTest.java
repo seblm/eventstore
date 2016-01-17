dc595b027ab146d2c164d2b11d42b16445a6c9d4
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2016-01-17 14:25:39 +0100

    tests are more readable

--- src/test/java/name/lemerdy/sebastian/eventstore/EventStoreTest.java
+++ src/test/java/name/lemerdy/sebastian/eventstore/EventStoreTest.java
@@ -3,70 +3,67 @@ package name.lemerdy.sebastian.eventstore;
 import org.junit.Test;
 
 import java.time.Instant;
-import java.time.ZoneId;
 import java.util.List;
 
-import static java.time.Clock.fixed;
 import static java.time.Instant.now;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.groups.Tuple.tuple;
 
 public class EventStoreTest {
 
     @Test
     public void should_store_event() {
         Instant now = now();
-        EventStore eventStore = new EventStore(fixed(now, ZoneId.systemDefault()));
+        EventStore eventStore = new EventStore(new IncrementingClock(now));
 
         eventStore.store("name.lemerdy.sebastian.type", "data");
 
-        List<Event> events = eventStore.events();
-        assertThat(events).containsExactly(new Event(now, "name.lemerdy.sebastian.type", "data"));
+        assertThat(eventStore.events()).containsExactly(
+                new Event(now, "name.lemerdy.sebastian.type", "data"));
     }
 
     @Test
     public void should_retrieve_all_events_in_chronological_order() {
-        EventStore eventStore = new EventStore(new IncrementingClock(now()));
-        eventStore.store("name.lemerdy.sebastian.type", "data_1");
-        eventStore.store("name.lemerdy.sebastian.type", "data_2");
+        Instant now = now();
+        EventStore eventStore = new EventStore(new IncrementingClock(now))
+                .store("name.lemerdy.sebastian.type", "data_1")
+                .store("name.lemerdy.sebastian.type", "data_2");
 
         List<Event> events = eventStore.events();
 
-        assertThat(events).extracting(Event::getType, Event::getData).containsExactly(
-                tuple("name.lemerdy.sebastian.type", "data_1"),
-                tuple("name.lemerdy.sebastian.type", "data_2"));
+        assertThat(events).containsExactly(
+                new Event(now.plusSeconds(0), "name.lemerdy.sebastian.type", "data_1"),
+                new Event(now.plusSeconds(1), "name.lemerdy.sebastian.type", "data_2"));
     }
 
 
     @Test
     public void should_retrieve_all_events_for_a_given_type() {
-        EventStore eventStore = new EventStore(new IncrementingClock(now()));
-        eventStore.store("name.lemerdy.sebastian.type1", "data_1");
-        eventStore.store("name.lemerdy.sebastian.type2", "data_2");
-        eventStore.store("name.lemerdy.sebastian.type1", "data_3");
+        Instant now = now();
+        EventStore eventStore = new EventStore(new IncrementingClock(now))
+                .store("name.lemerdy.sebastian.typeA", "data_0")
+                .store("name.lemerdy.sebastian.typeB", "data_1")
+                .store("name.lemerdy.sebastian.typeA", "data_2");
 
-        List<Event> events = eventStore.events("name.lemerdy.sebastian.type1");
+        List<Event> events = eventStore.events("name.lemerdy.sebastian.typeA");
 
-        assertThat(events).extracting(Event::getType, Event::getData).containsExactly(
-                tuple("name.lemerdy.sebastian.type1", "data_1"),
-                tuple("name.lemerdy.sebastian.type1", "data_3")
-        );
+        assertThat(events).extracting(Event::getType)
+                .allMatch("name.lemerdy.sebastian.typeA"::equals)
+                .hasSize(2);
     }
 
     @Test
     public void should_retrieve_all_events_starting_from_some_instant() {
         Instant now = now();
-        EventStore eventStore = new EventStore(new IncrementingClock(now));
-        eventStore.store("name.lemerdy.sebastian.type", "data_1");
-        eventStore.store("name.lemerdy.sebastian.type", "data_2");
-        eventStore.store("name.lemerdy.sebastian.type", "data_3");
+        EventStore eventStore = new EventStore(new IncrementingClock(now))
+                .store("name.lemerdy.sebastian.type", "data_0")
+                .store("name.lemerdy.sebastian.type", "data_1")
+                .store("name.lemerdy.sebastian.type", "data_2");
 
         List<Event> events = eventStore.events(now.plusSeconds(1));
 
-        assertThat(events).extracting(Event::getType, Event::getData).containsExactly(
-                tuple("name.lemerdy.sebastian.type", "data_2"),
-                tuple("name.lemerdy.sebastian.type", "data_3")
-        );
+        assertThat(events).containsExactly(
+                new Event(now.plusSeconds(1), "name.lemerdy.sebastian.type", "data_1"),
+                new Event(now.plusSeconds(2), "name.lemerdy.sebastian.type", "data_2"));
     }
 
 }
